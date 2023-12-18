15a2873b793092b1a3539b6cebd550554cc35e1f
iamkralb <peter.augustinak@gmail.com>
2023-12-18 09:06:44 +0100

    feat: 62 (mechanics of state), 63 (adding another piece of state), 64 (react dev tools), 65 (state updated on current state), 66 (state guidlines)

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -8,43 +8,54 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1); // it sets initial value
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if(step > 1) {
-      setStep(step - 1 );
+      setStep((currenStep) => currenStep - 1);
     }
   }
 
   function handleNext() {
     if(step < 3) {
-      setStep(step + 1);
+      setStep((currenStep) => currenStep + 1);
     }
   }
 
-  return <div className="steps">
-    <div className="numbers">
-      <div className={step >= 1 ? "active" : ""}>1</div>
-      <div className={step >= 2 ? "active" : ""}>2</div>
-      <div className={step >= 3 ? "active" : ""}>3</div>
-    </div>
+  return (
+  <>
+    <button className="close" onClick={
+      () => setIsOpen((open) => !open)}>&times;
+    </button>
 
-    <p className="message">Step {step}: {messages[step-1]}</p>
+    {isOpen && (
+      <div className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
 
-    <div className="buttons">
-      <button
-          style={{backgroundColor: "#7950f2", color: "#fff"}}
-          onClick={handlePrevious}  // must be just stated, not called directly
-      >
-        Previous
-      </button>
+        <p className="message">Step {step}: {messages[step-1]}</p>
 
-      <button
-          style={{backgroundColor: "#7950f2", color: "#fff"}}
-          onClick={handleNext}  // must be just stated, not called directly
-      >
-        Next
-      </button>
+        <div className="buttons">
+          <button
+              style={{backgroundColor: "#7950f2", color: "#fff"}}
+              onClick={handlePrevious}  // must be just stated, not called directly
+          >
+            Previous
+          </button>
 
-    </div>
-  </div>
+          <button
+              style={{backgroundColor: "#7950f2", color: "#fff"}}
+              onClick={handleNext}  // must be just stated, not called directly
+          >
+            Next
+          </button>
+
+        </div>
+      </div>
+      )}
+  </>
+  )
 }
