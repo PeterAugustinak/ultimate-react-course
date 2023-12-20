10d0e77a98a27f0c7cbbde514053e5aeed42fa17
iamkralb <peter.augustinak@gmail.com>
2023-12-20 11:10:21 +0100

    chore: css cleaning, comments

--- challenge-02-date-counter/src/App.js
+++ challenge-02-date-counter/src/App.js
@@ -10,10 +10,10 @@ export default function App() {
 }
 
 function Counter() {
-  const [step, setStep] = useState(1); // it sets initial value
+  const [step, setStep] = useState(1);
   const [count, setCount] = useState(1);
 
-  // specific for counter
+  // step handlers
   function handleStepBackward() {
       if(step>1) {
           setStep((step) => step - 1);
@@ -23,6 +23,7 @@ function Counter() {
       setStep((step) => step + 1);
   }
 
+  // count handlers
   function handleCountBackward() {
       setCount((count) => count - 1);
   }
@@ -30,8 +31,11 @@ function Counter() {
       setCount((count) => count + 1);
   }
 
+  // compute sum value from today
   const fromToday = step * count
-  const currentDate = new Date();
+
+  // compute and format date
+    const currentDate = new Date();
   const modifiedDate = new Date(currentDate);
   modifiedDate.setDate(currentDate.getDate() + fromToday);
 
@@ -42,35 +46,37 @@ function Counter() {
       day: 'numeric'
     };
 
-    const displayDate = new Date(modifiedDate).toLocaleDateString(
+  const displayDate = new Date(modifiedDate).toLocaleDateString(
         'en-US', options
-    );
+  );
 
   return (
       <>
-      <div className="counter">
-          <Btn arrow="-" handle={handleStepBackward}></Btn>
-          <p>Step: <b>{step}</b></p>
-          <Btn arrow="+" handle={handleStepForward}></Btn>
-      </div>
-        <div className="counter">
-          <Btn arrow="-" handle={handleCountBackward}></Btn>
-            <p>Count: <b>{count}</b></p>
-          <Btn arrow="+" handle={handleCountForward}></Btn>
-      </div>
+          <div className="counter">
+              <Btn arrow="-" handle={handleStepBackward}></Btn>
+              <p>Step: <b>{step}</b></p>
+              <Btn arrow="+" handle={handleStepForward}></Btn>
+          </div>
+
+          <div className="counter">
+              <Btn arrow="-" handle={handleCountBackward}></Btn>
+                <p>Count: <b>{count}</b></p>
+              <Btn arrow="+" handle={handleCountForward}></Btn>
+          </div>
+
           <div className="counter">
               <h3>{fromToday} day(s) from today is {displayDate}</h3>
           </div>
-          </>
+      </>
   )
 }
 
-function Btn({arrow, handle}) {
+function Btn({arrow, handle: handler}) {
     return (
         <div className="buttons">
           <button
               style={{backgroundColor: "#7950f2", color: "#fff"}}
-              onClick={handle}
+              onClick={handler}
           >
           {arrow}
          </button>
