c1c0fe2048345e5ce45e81c3ce891a8a1306d095
iamkralb <peter.augustinak@gmail.com>
2023-12-21 10:53:17 +0100

    feat: 73 (controling elements), 74 (state vs props)

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,16 +22,35 @@ function Logo() {
 }
 
 function Form() {
+    const defaultQuantity = 1;
+    const defaultDescription = "";
+
+    const [quantity, setQuantity] = useState(defaultQuantity);
+    const [description, setDescription] = useState(defaultDescription);
+
     function handleSubmit(e) {  // typical name of form submitting function, e = event
         e.preventDefault(); // preventing reload of the page on submit
         // console.log(e)
+        if (!description) return;
+
+        // creating new object based on state values
+        const newItem = { description, quantity, packed: false, id: Date.now() };
+        console.log(newItem);
+
+        // after form is submitted, we want to reset fields to default
+        setQuantity(defaultQuantity)
+        setDescription(defaultDescription)
     }
-    // usig handleSubmit on form is better than handleClick on button as this way it
+
+    // using handleSubmit on form is better than handleClick on button as this way it
     // also works with using ENTER
     return (
         <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your trip?</h3>
-            <select>
+            <select
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+            >
                 {Array.from({length: 20}, (_, i) => i + 1).map
                 ((num) => (
                     <option value={num} key={num}>
@@ -39,7 +60,12 @@ function Form() {
 
                 }
             </select>
-            <input type="text" placeholder="Item ..."/>
+            <input
+                type="text"
+                placeholder="Item ..."
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
             <button>Add</button>
 
         </form>
