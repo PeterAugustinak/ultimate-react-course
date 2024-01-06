31f7454406800fc73fb7de48a68ce3a7f0e7a8c8
iamkralb <peter.augustinak@gmail.com>
2024-01-06 19:58:58 +0100

    feat: refinement of the tip calculator app

--- challenge-tip-calculator/src/App.js
+++ challenge-tip-calculator/src/App.js
@@ -3,33 +3,32 @@ import {useState} from "react";
 
 export default function App() {
     const [bill, setBill] = useState(0);
-    const [service, setService] = useState(10);
-    const [friendService, setFriendService] = useState(10);
+    const [service, setService] = useState(0);
+    const [friendService, setFriendService] = useState(0);
 
-    function handleSetBill(value) {
-        setBill(value);
+    function handleReset() {
+        setBill(0);
+        setService(0);
+        setFriendService(0);
     }
 
-    function handleSetService(value) {
-        setService(value);
-    }
-
-    function handleSetFriendService(value) {
-        setFriendService(value);
-    }
-
-    var tip = bill / 100 * (Number(service) + Number(friendService))
-    var total = Number(bill) + tip
+    const tip = bill / 100 * ((service + friendService) / 2)
+    const total = bill + tip
 
     return (
         <div>
             <Form
                 bill={bill}
-                onSetBill={handleSetBill}
-                onSetService={handleSetService}
-                onSetFriendService={handleSetFriendService}
+                onSetBill={setBill}
+                onSetService={setService}
+                onSetFriendService={setFriendService}
             />
-            <Count bill={bill} tip={tip} total={total} onReset={setBill}/>
+            {bill > 0 && (
+            <>
+                <Output bill={bill} tip={tip} total={total}/>
+                <Reset onReset={handleReset}/>
+            </>
+            )}
         </div>
     )
 }
@@ -39,58 +38,66 @@ export function Form({bill, onSetBill, onSetService, onSetFriendService}) {
 
     return (
         <div>
-            <FormItem
-                bill={bill}
-                onSetBill={onSetBill}
-                question="How much was the bill?"
-            />
-            <FormItemChoice
-                onSet={onSetService}
-                question="How did you like the service?"
-            />
-            <FormItemChoice
-                onSet={onSetFriendService}
-                question="How much your friends like the service?"
-            />
+            <BillInput bill={bill} onSetBill={onSetBill}>
+                How much was the bill?
+            </BillInput>
+
+            <SelectPercentage onSelect={onSetService}>
+                How did you like the service?
+            </SelectPercentage>
+
+            <SelectPercentage onSelect={onSetFriendService}>
+                How much your friends like the service?
+            </SelectPercentage>
+
             <hr/>
         </div>
     )
 }
 
-export function FormItem({bill, onSetBill, question}) {
+
+export function BillInput({bill, onSetBill, children}) {
     return (
         <div>
-            <p>
-                {question} <input type="text" value={bill} onChange={e=>onSetBill(e.target.value)}/>
-            </p>
-
+            <label>{children} </label>
+            <input
+                type="text"
+                value={bill}
+                onChange={e=>onSetBill(Number(e.target.value))}/>
         </div>
     )
 }
 
-export function FormItemChoice({onSet, question}) {
+
+export function SelectPercentage({onSelect, children}) {
     return (
         <div>
-            <p>{question} <select onChange={e=>onSet(e.target.value)}>
+            <label>{children} </label>
+            <select onChange={e=>onSelect(Number(e.target.value))}>
                 <option value="0">Dissatisfied (0%)</option>
                 <option value="5">It was okay (5%)</option>
                 <option value="10">It was good (10%)</option>
                 <option value="20">Absolutely amazing (20%)</option>
             </select>
+        </div>
+    )
+}
+
+
+export function Output({bill, tip, total}) {
+    return (
+        <div>
+            <p>
+                <strong>You pay ${total} </strong>
+                (${bill} bill + ${tip} tip)
             </p>
         </div>
     )
 }
 
 
-export function Count({bill, tip, total, onReset}) {
+export function Reset({onReset}) {
     return (
-        <div>
-            <p>
-                <strong>You pay ${total} </strong>
-                (${bill} bill + ${tip} tip)</p>
-            <button onClick={() => onReset("0")}>Reset</button>
-        </div>
+        <button onClick={onReset}>Reset</button>
     )
-
 }
