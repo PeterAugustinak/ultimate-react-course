a4de5e24713b5f88badc0de75396eb4ad00fc614
kralb <iamkralb@gmail.com>
2024-03-02 08:15:32 +0100

    feat: 182 (child to parent communication), 183 (lifecycle methods)

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -35,14 +35,17 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
     state = {
-        location: 'Spisska Nova Ves',
+        location: '',
         isLoading: false,
         displayLocation: "",
         weather: {},
     };
 
-    // async fetchWeather() {
+
     fetchWeather = async () => {
+        // do not attempt to search if at least two letters are imputed
+        if (this.state.location.length < 2) return this.setState({weather: {}});
+
         try {
             this.setState({isLoading: true});
 
@@ -68,30 +71,46 @@ class App extends React.Component {
             const weatherData = await weatherRes.json();
             this.setState({weather: weatherData.daily});
         } catch (err) {
-            console.err(err);
+            console.error(err);
         } finally {
             this.setState({isLoading: false});
         }
+    };
+
+    setLocation = (e) => this.setState(
+        {location: e.target.value}
+    )
+
+    // like useEffect with [] - runs on mount
+    componentDidMount() {
+        // we don't need it on page load as there is nothing to search
+        // this.fetchWeather();
+
+        // read from local storage on mount what will show the previous location
+        // (or nothing if first time)
+        this.setState({location: localStorage.getItem("location") || ""});
     }
+
+    // useEffect with dependencies
+    // useEffect [location] - not called on mount, only on component update
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.location !== prevState.location) {
+            this.fetchWeather();
+
+            // after each location found, store location to local storage
+            localStorage.setItem("location", this.state.location);
+        }
+    }
+
 
     render() {
         return (
             <div className='app'>
                 <h1>Classy Weather</h1>
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Search from location ..."
-                        value={this.state.location}
-                        onChange={
-                            (e) => this.setState(
-                                {location: e.target.value}
-                            )
-                        }
-                    />
-                </div>
-                <button onClick={this.fetchWeather}>Get weather</button>
-
+                <Input
+                    location={this.state.location}
+                    onChangeLocation={this.setLocation}
+                />
                 {this.state.isLoading && <p className={"loader"}>Loading ... </p>}
 
                 {this.state.weather.weathercode && (
@@ -108,8 +127,27 @@ class App extends React.Component {
 
 export default App;
 
+class Input extends React.Component {
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    placeholder="Search from location ..."
+                    value={this.props.location}
+                    onChange={this.props.onChangeLocation}
+                />
+            </div>
+        )
+    }
+}
+
 
 class Weather extends React.Component {
+    componentWillUnmount() {
+        console.log("Weather is unmounting");
+    }
+
     render() {
         const {
             temperature_2m_max: max,
