ecab3de2d810dca5da94d590c30b2a7520e64929
kralb <iamkralb@gmail.com>
2024-03-05 08:38:10 +0100

    feat: 189 (managing state with useReducer), 190 (the reacy quiz app), 191 (loading questions from fake api)

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,10 +1,51 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import React, {useEffect, useReducer} from "react";
 
+const initialState = {
+    questions: [],
+    // states: loading, error, ready, active, finished
+    status: 'loading',
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'dataReceived':
+            return {
+                ...state,
+                questions: action.payload,
+                status: "ready",
+            }
+        case "dataFailed":
+            return {
+                ...state,
+                status: "error",
+            }
+        default:
+            throw new Error("Action unknown");
+    }
+}
 
 export default function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    // read data from fake json server on mount
+    useEffect(() => {
+        fetch("http://localhost:8003/questions")
+            .then((res) => res.json())
+            .then((data) => dispatch({type: 'dataReceived', payload: data}))
+            .catch((err) => dispatch({type: 'dataFailed'}))
+    }, []);
+
     return (
-        <div>
-            <DateCounter/>
+        <div className="app">
+            <Header/>
+            <Main>
+                <p>1/15</p>
+                <p>Question?</p>
+            </Main>
+
+
         </div>
     )
 }
