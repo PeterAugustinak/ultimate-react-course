387235ad43fd855f4e6176e4c2288cd11742b6ed
kralb <iamkralb@gmail.com>
2024-03-06 07:41:13 +0100

    feat: 192 (handling loading, error and ready status), 193 (starting a new quiz), 194 (displaying questions)

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -1,11 +1,17 @@
 import Header from "./Header";
 import Main from "./Main";
 import React, {useEffect, useReducer} from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
 
 const initialState = {
     questions: [],
     // states: loading, error, ready, active, finished
     status: 'loading',
+    index: 0,
 }
 
 function reducer(state, action) {
@@ -21,13 +27,17 @@ function reducer(state, action) {
                 ...state,
                 status: "error",
             }
+        case "start":
+            return {...state, status: "active"}
         default:
             throw new Error("Action unknown");
     }
 }
 
 export default function App() {
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const [{questions, status, index}, dispatch] = useReducer(reducer, initialState);
+
+    const numQuestions = questions.length
 
     // read data from fake json server on mount
     useEffect(() => {
@@ -41,8 +51,13 @@ export default function App() {
         <div className="app">
             <Header/>
             <Main>
-                <p>1/15</p>
-                <p>Question?</p>
+                {status === "loading" && <Loader/>}
+                {status === "error" && <Error/>}
+                {status === "ready" &&
+                    <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>
+                }
+                {status === "active" && <Question question={questions[index]}/>}
+
             </Main>
 
 
