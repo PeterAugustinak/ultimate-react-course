bffdca48dff99badba8e384a70b8829ac8eaaf09
kralb <iamkralb@gmail.com>
2024-03-15 17:43:32 +0100

    feat: 206 (implementing Main Pages and Routes)

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,8 +1,19 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Product from "../pages/Product.jsx";
+import Pricing from "../pages/Pricing.jsx"
+import HomePage from "../pages/HomePage.jsx";
+import PageNotFound from "../pages/PageNotFound.jsx";
+
 const App = () => {
     return (
-        <div>
-            <h1>WorldWise</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="product" element={<Product/>}/>
+                <Route path="pricing" element={<Pricing/>}/>
+                <Route path="*" element={<PageNotFound/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
