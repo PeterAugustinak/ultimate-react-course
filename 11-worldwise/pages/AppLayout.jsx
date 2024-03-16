62d26da72359eefdec430b65323762ac8663a999
kralb <iamkralb@gmail.com>
2024-03-16 16:53:24 +0100

    feat: 207 (linking between routes with Link, NavLink), 208 (styling options for react application), 209 (using css modiles), 210 (building the pages)

---
+++ 11-worldwise/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import AppNav from "../components/AppNav.jsx";
+
+function AppLayout() {
+    return (
+        <div>
+            <AppNav/>
+            AAA
+
+        </div>
+    );
+}
+
+export default AppLayout;
