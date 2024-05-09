4cfa927eb44a56947049752060eda0a07eadb664
kralb <iamkralb@gmail.com>
2024-05-09 08:48:42 +0200

    feat: 264 (working with action creators), 265 (adding more state: customer), 266 (professional redix file structure: state slices), 267 (back to react: connecting our redux app with react)

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,52 +1,13 @@
-import {createStore} from "redux";
-
-const initialState = {
-    balance: 0,
-    loan: 0,
-    loanPurpose: "",
-};
-
-function reducer(state = initialState, action) {
-    switch (action.type) {
-        case "account/deposit":
-            return {...state, balance: state.balance + action.payload};
-        case "account/withdraw":
-            return {...state, balance: state.balance - action.payload};
-        case "account/requestLoan":
-            if (state.loan > 0) return state;
-            return {
-                ...state,
-                loan: action.payload.amount,
-                loanPurpose: action.payload.loanPurpose,
-                balance: state.balance + action.payload.amount,
-            };
-        case "account/payLoan":
-            return {
-                ...state,
-                loan: 0,
-                loanPurpose: "",
-                balance: state.balance - state.loan
-            };
-        default:
-            return state;
-
-    }
-}
-
-const store = createStore(reducer);
-
-store.dispatch({type: "account/deposit", payload: 500});
-store.dispatch({type: "account/withdraw", payload: 200});
-
-console.log(store.getState())
-
-store.dispatch({
-    type: "account/requestLoan",
-    payload: {loanPurpose: "Buy a car", amount: 4000}
-});
-console.log(store.getState())
-
-store.dispatch({type: "account/payLoan"});
-console.log(store.getState())
+import {combineReducers, createStore} from "redux";
+import accountReducer from "./feature/accounts/accountSlice";
+import customerReducer from "./feature/customers/customerSlice";
 
 
+const rootReducer = combineReducers({
+    account: accountReducer,
+    customer: customerReducer,
+})
+
+const store = createStore(rootReducer);
+
+export default store;
