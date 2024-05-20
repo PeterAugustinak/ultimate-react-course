85041cbd2c62faf2688f0c3f2e62ca7852ae1c2c
kralb <iamkralb@gmail.com>
2024-05-20 08:47:47 +0200

    feat: 288 (displaying a loading indicator), 289 (handling errors with error elements), 290 (fetchiOCng orders)

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -3,18 +3,31 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./ui/Home.jsx";
 import Menu, {loader as menuLoader} from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
-import Order from "./features/order/Order.jsx";
+import Order, {loader as loaderOrder} from "./features/order/Order.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
+import Error from "./ui/Error.jsx";
 
 const router = createBrowserRouter([
         {
-            element: <AppLayout/>, children: [
+            element: <AppLayout/>,
+            errorElement: <Error/>,
+            children: [
                 {path: '/', element: <Home/>},
-                {path: '/menu', element: <Menu/>, loader: menuLoader},
+                {
+                    path: '/menu',
+                    element: <Menu/>,
+                    loader: menuLoader,
+                    errorElement: <Error/>,
+                },
                 {path: '/cart', element: <Cart/>},
                 {path: '/order/new', element: <CreateOrder/>},
-                {path: '/order/:orderId', element: <Order/>}
+                {
+                    path: '/order/:orderId',
+                    element: <Order/>,
+                    loader: loaderOrder,
+                    errorElement: <Error/>,
+                }
             ]
         }
     ]
