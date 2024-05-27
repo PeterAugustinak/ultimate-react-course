3426c673f1949e199bc4747487cc244fc08aa3a0
kralb <iamkralb@gmail.com>
2024-05-27 08:49:38 +0200

    feat: 298 (the box model: spacing, borders and display), 299 (responsive design), 300 (using flexbox), 301 (using css grid)

--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -3,7 +3,10 @@ import SearchOrder from "../features/order/SearchOrder.jsx";
 
 function Header() {
     return (
-        <header className="bg-yellow-500 uppercase">
+        <header className="
+        flex items-center justify-between
+        bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 sm:px-6
+        ">
             <Link to="/" className="tracking-widest">Fast React Pizza co.</Link>
             <SearchOrder/>
             <p>Hello!</p>
