93c9425c6d33c31780a5ecf25328f9e0f5c4e9bb
kralb <iamkralb@gmail.com>
2024-06-18 08:54:08 +0200

    feat: 332 (building more reusable styled components), 333 (setting up pages and routes)

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,28 +1,29 @@
-import styled from "styled-components";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Login from "./pages/Login.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import Settings from "./pages/Settings.jsx";
+import Users from "./pages/Users.jsx";
+import Bookings from "./pages/Bookings.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
-import Button from "./ui/Button.jsx";
-import Input from "./ui/Input.jsx";
-import Heading from "./ui/Heading.jsx";
-
-
-const StyledApp = styled.div`
-    background-color: orangered;
-    padding: 20px;
-`
 
 function App() {
     return (
         <>
             <GlobalStyles/>
-            <StyledApp>
-                <Heading as="h1">The Wild Oasis</Heading>
-                <Heading as="h2">The Wild Oasis</Heading>
-                <Heading as="h3">
-                    <Button onClick={() => alert("Check in")}>Check In</Button>
-                    <Button onClick={() => alert("Check out")}>Check Out</Button>
-                </Heading>
-                <Input type="number" placeholder="Number of guests"/>
-            </StyledApp>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Navigate replace to="dashboard"/>}/>
+                    <Route path="dashboard" element={<Dashboard/>}/>
+                    <Route path="dashboard" element={<Bookings/>}/>
+                    <Route path="dashboard" element={<Cabins/>}/>
+                    <Route path="dashboard" element={<Login/>}/>
+                    <Route path="dashboard" element={<PageNotFound/>}/>
+                    <Route path="dashboard" element={<Settings/>}/>
+                    <Route path="dashboard" element={<Users/>}/>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
