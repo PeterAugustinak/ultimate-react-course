b752299d7171cf4c765203bea4fe41e9fa29d483
kralb <iamkralb@gmail.com>
2024-07-22 09:05:37 +0200

    feat: 345 (section overview), 346 (what is react query?), 347 (setting up react query), 349 (fetching cabon data)

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -8,26 +8,40 @@ import Users from "./pages/Users.jsx";
 import Bookings from "./pages/Bookings.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
 import AppLayout from "./ui/AppLayout.jsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient(
+    {
+        defaultOptions: {
+            queries: {
+                staleTime: 60 * 1000,
+            }
+        }
+    }
+);
 
 function App() {
     return (
-        <>
-            <GlobalStyles/>
-            <BrowserRouter>
-                <Routes>
-                    <Route element={<AppLayout/>}>
-                        <Route index element={<Navigate replace to="dashboard"/>}/>
-                        <Route path="dashboard" element={<Dashboard/>}/>
-                        <Route path="bookings" element={<Bookings/>}/>
-                        <Route path="cabins" element={<Cabins/>}/>
-                        <Route path="*" element={<PageNotFound/>}/>
-                        <Route path="settings" element={<Settings/>}/>
-                        <Route path="users" element={<Users/>}/>
-                    </Route>
-                    <Route path="login" element={<Login/>}/>
-                </Routes>
-            </BrowserRouter>
-        </>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false}>
+                <GlobalStyles/>
+                <BrowserRouter>
+                    <Routes>
+                        <Route element={<AppLayout/>}>
+                            <Route index element={<Navigate replace to="dashboard"/>}/>
+                            <Route path="dashboard" element={<Dashboard/>}/>
+                            <Route path="bookings" element={<Bookings/>}/>
+                            <Route path="cabins" element={<Cabins/>}/>
+                            <Route path="*" element={<PageNotFound/>}/>
+                            <Route path="settings" element={<Settings/>}/>
+                            <Route path="users" element={<Users/>}/>
+                        </Route>
+                        <Route path="login" element={<Login/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </ReactQueryDevtools>
+        </QueryClientProvider>
     );
 }
 
